2fe34472f92ea33e55a9fe1d2ae50d13736fa8a9
praveen <praveen@praveen-Aspire-5740>
2013-03-09 17:50:27 +0530

    Eight Puzzles tested working.

--- src/test/AstarTest.java
+++ src/test/AstarTest.java
@@ -5,7 +5,6 @@
 package test;
 
 import eightiles.EightTiles;
-import eightiles.EightTilesNode;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,8 +16,20 @@ public class AstarTest {
     
     public static void main(String[] args)
     {
-        EightTiles E = new EightTiles();
-        //EightTiles E = new EightTiles(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,EightTilesNode.EMPTY_TILE)));
+        //EightTiles E = new EightTiles(EightTiles.MANHATTAN);
+        EightTiles E = new EightTiles(
+                new ArrayList<>(Arrays.asList(6,5,0,8,7,4,2,1,3)), 
+                EightTiles.DISPLACED_TILES);
+        
+        // search using displaced tiles heuristic
+        System.out.println("Searching using displaced tiles heuristic");
+        System.out.println("-----------------------------------------");
+        E.search();
+        
+        // search using manhattan distance heuristic
+        System.out.println("Searching using manhattan distance heuristic");
+        System.out.println("--------------------------------------------");
+        E.setHeuristic(EightTiles.MANHATTAN);
         E.search();
     }
     
